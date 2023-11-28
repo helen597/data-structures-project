7335100d906c7a6f4397432d01f39868d3ed2fd7
Olga Semenova <helen597597@gmail.com>
2023-11-28 20:14:53 +0500

    edit linked_list.py, test_linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,38 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data, None)
+        if not self.head:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            next_node = self.head
+            self.head = new_node
+            self.head.next_node = next_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data, None)
+        if not self.tail:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +42,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,29 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import pytest
+from src.linked_list import LinkedList, Node
+
+
+@pytest.fixture
+def node1():
+    return Node({'id': 1})
+
+
+def test_node_init(node1):
+    assert Node({'id': 1}).data == {'id': 1}
+    assert node1.next_node == None
+
+
+def test_linked_list():
+    ll = LinkedList()
+    assert str(ll) == 'None'
+    ll.insert_beginning({'id': 1})
+    assert ll.head.data == {'id': 1}
+    assert ll.tail.data == {'id': 1}
+    ll.insert_at_end({'id': 2})
+    ll.insert_at_end({'id': 3})
+    ll.insert_beginning({'id': 0})
+    assert str(ll) == "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+    ll = LinkedList()
+    ll.insert_at_end({'id': 1})
+    assert ll.head.data == {'id': 1}
+    assert ll.tail.data == {'id': 1}
